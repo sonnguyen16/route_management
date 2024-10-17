9e1fb10088851bc85ba67e2cf52b0f339ffe6a29
giangteam <giang.bvu.it@gmail.com>
2024-10-17 17:17:49 +0700

    update

--- resources/js/Pages/Map/Index.vue
+++ resources/js/Pages/Map/Index.vue
@@ -16,16 +16,18 @@ const mapContainer = ref();
 let map: any = null;
 
 const coordStreet = [
+   
     [106.65656967668332, 10.788345590010692],
-  /*  [106.65633981621085, 10.787891601865596],
+    [106.65633981621085, 10.787891601865596],
     [106.65627062925398, 10.78783981961],
     [106.65616849612803, 10.787800982912955],
     [106.65611248763867, 10.787775091779537],
     [106.65607624685282, 10.787619744926772],
     [106.65604330068174, 10.787506471130058],
     [106.65601364912965, 10.787389960893677],
-    [106.65601035451272, 10.78711163070352], */
+    [106.65601035451272, 10.78711163070352], 
     [106.6560498899155, 10.786885082683582],
+    [106.65716, 10.78729],
 ];
 
 const geojsonLine = {
@@ -58,13 +60,12 @@ const geojsonImage = {
         },
     ],
 };
-
 onMounted(() => {
     map = new mapboxgl.Map({
         container: mapContainer.value,
         style: "mapbox://styles/mapbox/streets-v12",
         center: [106.65656967668332, 10.788345590010692],
-        zoom: 16,
+        zoom: 17,
     });
 
     map.on("style.load", function () {
@@ -93,10 +94,11 @@ onMounted(() => {
                 "line-color": [
                     "case",
                     ["boolean", ["feature-state", "click"], false],
-                    "#64bdbb",
-                    "#888888",
+                    "#e81717", // màu khi click
+                    "#0de04d", // màu khi load
                 ],
-                "line-width": 10,
+                "line-width": 10, // độ rộng của line
+                'line-opacity': 0.4, // độ mờ trong suốt của line
             },
         });
     /*
@@ -115,19 +117,50 @@ onMounted(() => {
         });
 */
         map.addLayer({
-            'id': 'route-label',
+            'id': 'duong-van-co',
             'type': 'symbol',
             'source': 'route',
             'layout': {
-                'symbol-placement': 'line-center',
-                'text-field': 'aa'
-            }
+                "icon-image": "demo",
+                "icon-size": 1,
+                "icon-allow-overlap": true,
+                'symbol-placement': 'line', // line-center, line, point // hiển thị nhiều lần hay ít lần
+                'text-field': "Phạm Ngũ Lão",
+                "text-size": 12,   
+                'text-max-width': 10,   
+                "symbol-spacing": 2,
+                "text-rotation-alignment": "map",
+                "text-keep-upright": false
+              //  'text-font': ['Open Sans Bold'], // font chữ đậm nhạt
+            //  "text-transform": "uppercase", chữ hoa
+               // "text-size": ["step", ["zoom"], 8, 16, 15],
+                // "text-offset": [0, 0.6], hiện chữ trên line hay dưới line
+            //    "text-overlap": "cooperative", // to
+            },
+           /* layout: {
+                'symbol-placement': 'line',
+          'text-size': ['get', 10],
+          'text-font': ['Open Sans Regular'],
+          'text-offset': [0.85, -1.7],
+          'text-anchor': 'top',
+          'text-max-width': 8,
+          'text-field': 'Pham Ngu Lao'
+        }, */
+            paint: {
+            "text-color": "blue", // màu của chữ
+            "text-halo-color": "white", // đường viền của chữ
+            "text-halo-width": 1, // độ đậm của đường viền
+            'line-opacity':     0.5,
+            'line-dasharray':   [0, 2], 
+            },
+            
         });
 
 
         let streetId: any = null;
         map.on("click", function () {
             if (streetId !== null) {
+                console.log(streetId);
                 map.setFeatureState(
                     { source: "route", id: streetId },
                     { click: false },
@@ -143,13 +176,13 @@ onMounted(() => {
                 .addTo(map);
 
             if (e.features.length > 0) {
-                streetId = e.features[0].id;
+                streetId = e.features[0].id;               
                 map.setFeatureState({ source: "route", id: streetId }, { click: true });
             }
         });
 
         // Load hình
-        map.loadImage("/public/cat.png", (error: any, image: any) => {
+        map.loadImage("/Km3.png", (error: any, image: any) => {
             console.log(error, image);
             if (error) throw error;
             map.addImage("cat", image);
@@ -179,7 +212,7 @@ onMounted(() => {
                     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
                 }
 
-                popup.setLngLat(coordinates).setHTML("Con mèo").addTo(map);
+                popup.setLngLat(coordinates).setHTML("KM 31+").addTo(map);
             });
 
             map.on("mouseleave", "hinh-anh", () => {
