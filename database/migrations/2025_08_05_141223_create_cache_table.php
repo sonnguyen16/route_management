31f3f799ad395107c31e6b85b85fd90a229ccf92
sonnguyen16 <sonnv2212@gmail.com>
2025-08-05 22:08:13 +0700

    update

--- database/migrations/2025_08_05_141223_create_cache_table.php
+++ database/migrations/2025_08_05_141223_create_cache_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('cache', function (Blueprint $table) {
-            $table->string('key')->primary();
+            $table->string('key');
             $table->mediumText('value');
             $table->integer('expiration');
         });
 
         Schema::create('cache_locks', function (Blueprint $table) {
-            $table->string('key')->primary();
+            $table->string('key');
             $table->string('owner');
             $table->integer('expiration');
         });
