dc20c29ddd4c64c52db0e9fd5b0091e9e48516f6
sonnguyen16 <sonnv2212@gmail.com>
2025-08-05 22:37:38 +0700

    Update

--- resources/js/Pages/Location/Index.vue
+++ resources/js/Pages/Location/Index.vue
@@ -83,12 +83,16 @@ const initializeWebSocket = () => {
   if (usePage().props.auth.user.role === 'leader') {
     window.Pusher = Pusher
 
+    const port = import.meta.env.VITE_REVERB_PORT ?? (
+      (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https' ? 443 : 8080
+    )
+
     echo = new Echo({
       broadcaster: 'reverb',
       key: import.meta.env.VITE_REVERB_APP_KEY,
       wsHost: import.meta.env.VITE_REVERB_HOST ?? 'localhost',
-      wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-      wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+      wsPort: port,
+      wssPort: port,
       forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
       enabledTransports: ['ws', 'wss'],
       auth: {
